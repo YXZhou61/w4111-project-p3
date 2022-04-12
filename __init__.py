81290471218c629e36b5c2e4970e938635d96cf5
YXZhou61 <60079494+YXZhou61@users.noreply.github.com>
2022-04-11 22:07:27 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ def create_app():
     #app.config['SECRET_KEY'] = 'secret'
     #app.config['SQLALCHEMY_DATABASE_URI'] = DATABASEURI
 
-    db.init_app(app)
+    #db.init_app(app)
     
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
